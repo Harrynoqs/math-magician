2b53795a3d7a3ba7e40139feaf45d5bce42c6e27
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-11-03 13:21:09 +0300

    Add Hooks refactor

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.buttonClick = this.buttonClick.bind(this);
+function Calculator() {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
+  function buttonClick(event) {
+    setState((state) => calculate(state, event.target.innerText));
   }
 
-  buttonClick(event) {
-    this.setState((state) => calculate(state, event.target.innerText));
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calc-container">
-        <div className="result-preview">
-          { total }
-          { operation }
-          { next }
-        </div>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>AC</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>+/-</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>%</button>
-        <button type="button" className="buttonKey colored" onClick={this.buttonClick}>÷</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>7</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>8</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>9</button>
-        <button type="button" className="buttonKey colored" onClick={this.buttonClick}>x</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>4</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>5</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>6</button>
-        <button type="button" className="buttonKey colored" onClick={this.buttonClick}>-</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>1</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>2</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>3</button>
-        <button type="button" className="buttonKey colored" onClick={this.buttonClick}>+</button>
-        <button type="button" className="buttonKey-2" onClick={this.buttonClick}>0</button>
-        <button type="button" className="buttonKey" onClick={this.buttonClick}>.</button>
-        <button type="button" className="buttonKey colored" onClick={this.buttonClick}>=</button>
+  const { total, next, operation } = state;
+  return (
+    <div className="calc-container">
+      <div className="result-preview">
+        { total }
+        { operation }
+        { next }
       </div>
-    );
-  }
+      <button type="button" className="buttonKey" onClick={buttonClick}>AC</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>+/-</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>%</button>
+      <button type="button" className="buttonKey colored" onClick={buttonClick}>÷</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>7</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>8</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>9</button>
+      <button type="button" className="buttonKey colored" onClick={buttonClick}>x</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>4</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>5</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>6</button>
+      <button type="button" className="buttonKey colored" onClick={buttonClick}>-</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>1</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>2</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>3</button>
+      <button type="button" className="buttonKey colored" onClick={buttonClick}>+</button>
+      <button type="button" className="buttonKey-2" onClick={buttonClick}>0</button>
+      <button type="button" className="buttonKey" onClick={buttonClick}>.</button>
+      <button type="button" className="buttonKey colored" onClick={buttonClick}>=</button>
+    </div>
+  );
 }
 
 export default Calculator;
