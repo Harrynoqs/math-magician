79e3ae279b12c052f007aaa4c5cdf6a3d6b71da0
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-11-09 09:33:28 +0300

    Added home.js

---
+++ src/components/Home.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <h2 className="home-title">Welcome to our page!</h2>
+    <p className="text">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nibh nisl,
+      hendrerit sit amet lectus nec,
+      placerat placerat erat.
+      Praesent ullamcorper nunc hendrerit aliquam ornare. Nam id sapien sed dui gravida
+      feugiat eget nec libero.
+      Duis nec aliquet nisi.
+      Vestibulum placerat sollicitudin magna, nec mollis neque tempor at.
+      Proin pretium accumsan massa.
+      Proin commodo purus arcu,
+      non semper neque bibendum et. Mauris vitae accumsan quam. Maecenas vitae purus non
+      enim efficitur mollis.
+      Vestibulum malesuada
+      quis magna vel molestie. Morbi auctor aliquet odio. Etiam accumsan bibendum turpis in congue.
+      Nunc in viverra felis, eu malesuada
+      elit. Integer pharetra sapien at mi posuere, quis porta nibh malesuada.
+
+      In pretium maximus leo, ut dictum felis cursus eget. Cras varius mi sit amet eros pretium,
+      at finibus eros facilisis.
+      Pellentesque
+      habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+      Maecenas pulvinar ultrices orci,
+      venenatis dictum
+      nibh dignissim in. Proin facilisis et nulla non rutrum. Fusce blandit viverra neque.
+      Vestibulum rutrum scelerisque dolor, in
+      pharetra nisl egestas non. Aliquam sit amet ex eget nulla viverra ultricies id at augue.
+      Proin a diam odio. Aenean aliquam, ipsum
+      at molestie ultrices, est erat pretium odio, eu aliquam velit diam ac neque.
+      Phasellus a orci feugiat,
+      mattis tortor ut, euismod libero.
+
+    </p>
+  </>
+);
+
+export default Home;
