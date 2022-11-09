6f85072d48d02deca5d5a2b3eb4a85fea2549e0b
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-11-09 09:34:30 +0300

    Modified calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,31 +9,34 @@ function Calculator() {
 
   const { total, next, operation } = state;
   return (
-    <div className="calc-container">
-      <div className="result-preview">
-        { total }
-        { operation }
-        { next }
+    <div className="wrapper--container">
+      <p className="calculator--paragraph">Let&#39;s do some math!</p>
+      <div className="calc-container">
+        <div className="result-preview">
+          { total }
+          { operation }
+          { next }
+        </div>
+        <button type="button" className="buttonKey" onClick={buttonClick}>AC</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>+/-</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>%</button>
+        <button type="button" className="buttonKey colored" onClick={buttonClick}>÷</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>7</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>8</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>9</button>
+        <button type="button" className="buttonKey colored" onClick={buttonClick}>x</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>4</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>5</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>6</button>
+        <button type="button" className="buttonKey colored" onClick={buttonClick}>-</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>1</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>2</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>3</button>
+        <button type="button" className="buttonKey colored" onClick={buttonClick}>+</button>
+        <button type="button" className="buttonKey-2" onClick={buttonClick}>0</button>
+        <button type="button" className="buttonKey" onClick={buttonClick}>.</button>
+        <button type="button" className="buttonKey colored" onClick={buttonClick}>=</button>
       </div>
-      <button type="button" className="buttonKey" onClick={buttonClick}>AC</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>+/-</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>%</button>
-      <button type="button" className="buttonKey colored" onClick={buttonClick}>÷</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>7</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>8</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>9</button>
-      <button type="button" className="buttonKey colored" onClick={buttonClick}>x</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>4</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>5</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>6</button>
-      <button type="button" className="buttonKey colored" onClick={buttonClick}>-</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>1</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>2</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>3</button>
-      <button type="button" className="buttonKey colored" onClick={buttonClick}>+</button>
-      <button type="button" className="buttonKey-2" onClick={buttonClick}>0</button>
-      <button type="button" className="buttonKey" onClick={buttonClick}>.</button>
-      <button type="button" className="buttonKey colored" onClick={buttonClick}>=</button>
     </div>
   );
 }
